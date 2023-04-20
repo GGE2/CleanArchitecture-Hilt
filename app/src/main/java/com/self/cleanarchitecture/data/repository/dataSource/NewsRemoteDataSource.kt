b666c9710fdd73c07d26c6aec7c3f3dd3adbc0bf
kimjihun <jihoon_story@naver.com>
2023-04-20 13:48:10 +0900

    webview 연동

--- app/src/main/java/com/self/cleanarchitecture/data/repository/dataSource/NewsRemoteDataSource.kt
+++ app/src/main/java/com/self/cleanarchitecture/data/repository/dataSource/NewsRemoteDataSource.kt
@@ -4,7 +4,5 @@ import com.self.cleanarchitecture.data.model.APIResponse
 import retrofit2.Response
 
 interface NewsRemoteDataSource {
-    suspend fun getTopHeadlines(
-        country : String, page:Int
-    ): Response<APIResponse>
+    suspend fun getTopHeadlines(country : String, page:Int): Response<APIResponse>
 }
--- app/src/main/java/com/self/cleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/com/self/cleanarchitecture/presentation/MainActivity.kt
@@ -4,14 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.self.cleanarchitecture.R
 import com.self.cleanarchitecture.databinding.ActivityMainBinding
+import com.self.cleanarchitecture.presentation.adapter.NewsAdapter
+import com.self.cleanarchitecture.presentation.viewmodel.NewsViewModel
+import com.self.cleanarchitecture.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
 
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    @Inject
+    lateinit var newsAdapter: NewsAdapter
+    private lateinit var binding:ActivityMainBinding
+    lateinit var viewModel : NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,6 +32,10 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bnvNews.setupWithNavController(navController)
+
+        viewModel = ViewModelProvider(this,factory)
+            .get(NewsViewModel::class.java)
+
     }
 
 }
